3ecba5f4c37d268bb2ad55638d6707eea01d6d81
gumeeee <gaxco123@gmail.com>
2025-02-28 01:20:52 -0300

    feat: add react-icons and implementing logo component

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { VscCoffee } from "react-icons/vsc";
+
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size={"icon"} asChild>
+      <Link href="/">
+        <VscCoffee className="w-7 h-7" />
+      </Link>
+    </Button>
+  );
 }
