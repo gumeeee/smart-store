da43e5ff9b16a568412b54c7b26f3f9ef42f0d69
gumeeee <gaxco123@gmail.com>
2025-02-28 01:08:14 -0300

    feat: add implementation in navbar component and use in layout

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,3 +1,22 @@
-export default function navbar() {
-  return <div>navbar</div>;
+import Container from "../global/container";
+import CartButton from "./cart-button";
+import DarkMode from "./dark-mode";
+import LinksDropdown from "./links-dropdown";
+import Logo from "./logo";
+import NavSearch from "./nav-search";
+
+export default function NavBar() {
+  return (
+    <nav className="border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 flex-wrap py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <CartButton />
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </Container>
+    </nav>
+  );
 }
