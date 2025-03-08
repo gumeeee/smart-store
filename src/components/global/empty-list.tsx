4730cfd0b3407f9d7a0708f760d8ab9e7cba3b1f
gumeeee <gaxco123@gmail.com>
2025-03-07 22:16:05 -0300

    feat: add implementation in empty-list and in section-title components

--- src/components/global/empty-list.tsx
+++ src/components/global/empty-list.tsx
@@ -1,3 +1,11 @@
-export default function EmptyList() {
-  return <div>empty-list</div>;
+import { cn } from "@/lib/utils";
+
+export default function EmptyList({
+  heading = "No items  found.",
+  classname,
+}: {
+  heading?: string;
+  classname?: string;
+}) {
+  return <h2 className={cn("text-xl", classname)}>{heading}</h2>;
 }
--- src/components/global/section-title.tsx
+++ src/components/global/section-title.tsx
@@ -1,3 +1,12 @@
-export default function SectionFile() {
-  return <div>SectionFile</div>;
+import { Separator } from "../ui/separator";
+
+export default function SectionFile({ text }: { text: string }) {
+  return (
+    <div>
+      <h2 className="text-3xl font-medium tracking-tighter capitalize mb-8">
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 }
