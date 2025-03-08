28d152956989f8699dbf052028225e6517f4dc22
gumeeee <gaxco123@gmail.com>
2025-03-08 00:43:41 -0300

    feat: add implementation in favorite products components

--- src/components/home/featured-products.tsx
+++ src/components/home/featured-products.tsx
@@ -1,3 +1,17 @@
-export default function FeaturedProducts() {
-  return <div>FeaturedProducts</div>;
+import { fetchFeaturedProducts } from "@/utils/actions";
+import EmptyList from "../global/empty-list";
+import SectionTitle from "../global/section-title";
+import ProductsGrid from "../products/products-grid";
+
+export default async function FeaturedProducts() {
+  const products = await fetchFeaturedProducts();
+
+  if (products.length === 0) return <EmptyList />;
+
+  return (
+    <section className="pt-24">
+      <SectionTitle text="featured products" />
+      <ProductsGrid products={products} />
+    </section>
+  );
 }
--- src/components/products/favorite-toggle-button.tsx
+++ src/components/products/favorite-toggle-button.tsx
@@ -1,3 +1,14 @@
-export default function FavoriteToggleButton() {
-  return <div>FavoriteToggleButton</div>;
+import { FaHeart } from "react-icons/fa";
+import { Button } from "../ui/button";
+
+export default function FavoriteToggleButton({
+  productId,
+}: {
+  productId: string;
+}) {
+  return (
+    <Button size={"icon"} variant={"outline"} className="p-3 cursor-pointer">
+      <FaHeart />
+    </Button>
+  );
 }
