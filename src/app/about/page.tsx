97a41164ea1dd9acf6250401b9f39f02ee28254e
gumeeee <gaxco123@gmail.com>
2025-03-09 23:38:32 -0300

    feat: adding implementation in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,24 @@
-import React from "react";
-
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <section>
+      <h1
+        className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center 
+      text-4xl font-bold leading-none tracking-wide sm:text-6xl"
+      >
+        A cada entrega,
+        <span className="bg-primary py-4 px-4 rounded-lg tracking-widest text-white">
+          um sorriso garantido
+        </span>
+      </h1>
+      <p
+        className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto
+      text-muted-foreground"
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis rerum
+        placeat, minima ratione ex ipsa voluptate mollitia aliquam. Fuga, quos
+        quia perferendis atque vero dolores veritatis eveniet asperiores facere
+        quibusdam.
+      </p>
+    </section>
+  );
 }
