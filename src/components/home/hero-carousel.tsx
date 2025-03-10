19a7f7eccb8a6cd0617bf10abdcbbc67242db979
gumeeee <gaxco123@gmail.com>
2025-03-09 22:59:09 -0300

    feat: adding implementation in hero-carousel component and images to carousel

--- src/components/home/hero-carousel.tsx
+++ src/components/home/hero-carousel.tsx
@@ -1,3 +1,46 @@
+import herocarousel1 from "@/../public/images/hero-carousel-1.jpg";
+import herocarousel2 from "@/../public/images/hero-carousel-2.jpg";
+import herocarousel3 from "@/../public/images/hero-carousel-3.jpg";
+import herocarousel4 from "@/../public/images/hero-carousel-4.jpg";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
+
+const carouselImages = [
+  herocarousel1,
+  herocarousel2,
+  herocarousel3,
+  herocarousel4,
+];
+
 export default function HeroCarousel() {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="p-2">
+                  <Image
+                    src={image}
+                    alt="Image carousel"
+                    className="w-full h-[24rem] rounded-md object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }
