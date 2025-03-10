df43d0823058cdb7b9efa524619dc4d7eec1d1f3
gumeeee <gaxco123@gmail.com>
2025-03-09 21:50:29 -0300

    feat: adding implementation in hero component

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeroCarousel from "./hero-carousel";
+
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          Estamos mudando a maneira como as pessoas compram
+        </h1>
+        <p className="mt-8 max-w-xl leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          veniam hic dignissimos molestias, at vero ratione natus perspiciatis!
+          Unde, nesciunt!
+        </p>
+        <Button asChild size={"lg"} className="mt-10">
+          <Link href="/products">Nossos produtos</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
