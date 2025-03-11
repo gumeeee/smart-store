27254a2c1cf90ee17611ca59ae1038030a217664
gumeeee <gaxco123@gmail.com>
2025-03-11 00:04:37 -0300

    feat: add fetchAllproducts

--- src/components/products/products-container.tsx
+++ src/components/products/products-container.tsx
@@ -13,7 +13,7 @@ export default async function ProductsContainer({
   layout: string;
   search: string;
 }) {
-  const products = await fetchAllProducts();
+  const products = await fetchAllProducts({ search });
   const totalProducts = products.length;
   const searchTerms = search ? `&search${search}` : "";
 
