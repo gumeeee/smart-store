4e425ca0562de951a8b54459d7283d2aa9746f1d
gumeeee <gaxco123@gmail.com>
2025-03-11 00:03:46 -0300

    feat: add implementation logic to search products

--- src/components/navbar/nav-search.tsx
+++ src/components/navbar/nav-search.tsx
@@ -1,11 +1,46 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
 import { Input } from "../ui/input";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function NavSearch() {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString || ""
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams();
+
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+
+    replace(`/products?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchParams.get("search")]);
+
   return (
     <Input
       type="search"
       placeholder="Pesquisar produto..."
       className="max-w-xs dark:bg-muted"
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 }
