7f7630c555933dd9cc90b9344c72ca82c45a2ecb
gumeeee <gaxco123@gmail.com>
2025-03-11 00:04:02 -0300

    feat: adding suspense wrap in navsearch

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,13 +4,16 @@ import DarkMode from "./dark-mode";
 import LinksDropdown from "./links-dropdown";
 import Logo from "./logo";
 import NavSearch from "./nav-search";
+import { Suspense } from "react";
 
 export default function NavBar() {
   return (
     <nav className="border-b">
       <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 flex-wrap py-8">
         <Logo />
-        <NavSearch />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
         <div className="flex gap-4 items-center">
           <CartButton />
           <DarkMode />
