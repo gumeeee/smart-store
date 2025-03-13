c7826d6e9de08e95587f2432d647d423c85b2793
gumeeee <gaxco123@gmail.com>
2025-03-12 23:34:30 -0300

    refactor: remove console log in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 
 export default clerkMiddleware((auth, req) => {
-  console.log(isPublicRoute);
   if (!isPublicRoute(req)) {
     auth().protect();
   }
