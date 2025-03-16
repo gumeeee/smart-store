3fc150b197b44ee85c66896d691ce87dd37df3cb
gumeeee <gaxco123@gmail.com>
2025-03-15 23:20:39 -0300

    feat: adding implementation in SignOutLink component

--- src/components/navbar/signout-link.tsx
+++ src/components/navbar/signout-link.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
 export default function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { signOut } = useAuth();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(() => {
+        router.push("/");
+        toast.success("Logout realizado com sucesso");
+      });
+    } catch (error) {
+      toast.error("Falha ao sair da conta");
+    }
+  };
+
+  return (
+    <button onClick={handleLogout} className="w-full text-left">
+      Sair
+    </button>
+  );
 }
