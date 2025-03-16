112df7196156955bf2b0a88de8ae0f282ef9ac85
gumeeee <gaxco123@gmail.com>
2025-03-15 23:20:02 -0300

    feat: adding implementation in UserIcon component

--- src/components/navbar/user-icon.tsx
+++ src/components/navbar/user-icon.tsx
@@ -1,3 +1,25 @@
-export default function UserIcon() {
-  return <div>UserIcon</div>;
+import { LuUser } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+
+export default async function UserIcon() {
+  const user = await currentUser();
+
+  const hasImage = user?.hasImage;
+
+  if (user) {
+    if (hasImage) {
+      return (
+        <Image
+          src={user.imageUrl}
+          alt={user.username ?? "User profile"}
+          className="w-7 h-7 rounded-full object-cover"
+          width={20}
+          height={20}
+        />
+      );
+    }
+  }
+
+  return <LuUser className="w-7 h-7 bg-primary rounded-full text-white" />;
 }
