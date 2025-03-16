a4e0c745015925764e712182ee16cf8777aaded5
gumeeee <gaxco123@gmail.com>
2025-03-15 23:21:24 -0300

    feat: adding conditional drop down menu item with SignedIn SignedOut

--- src/components/navbar/links-dropdown.tsx
+++ src/components/navbar/links-dropdown.tsx
@@ -9,6 +9,9 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { links } from "@/utils/links";
 import { LuAlignCenter } from "react-icons/lu";
+import UserIcon from "./user-icon";
+import { SignInButton, SignedOut, SignedIn, SignUpButton } from "@clerk/nextjs";
+import SignOutLink from "./signout-link";
 
 export default function LinksDropdown() {
   return (
@@ -16,14 +19,34 @@ export default function LinksDropdown() {
       <DropdownMenuTrigger asChild>
         <Button variant={"outline"} className="flex gap-4 max-w-[100px]">
           <LuAlignCenter className="w-6 h-6" />
+          <UserIcon />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-44" align="center" sideOffset={10}>
-        {links.map((link) => (
-          <DropdownMenuItem key={link.href} className="capitalize w-full">
-            <Link href={link.href}>{link.label}</Link>
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode="modal">
+              <button className="w-full text-left">Login</button>
+            </SignInButton>
           </DropdownMenuItem>
-        ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode="modal">
+              <button className="w-full text-left">Registrar-se</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => (
+            <DropdownMenuItem key={link.href} className="capitalize w-full">
+              <Link href={link.href}>{link.label}</Link>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
