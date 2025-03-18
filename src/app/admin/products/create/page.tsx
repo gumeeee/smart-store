683ae0a04e55485d26b9424c0b16e8301cbf5d12
gumeeee <gaxco123@gmail.com>
2025-03-17 22:07:51 -0300

    feat: adding form-input component and implementing in create product admin route

---
+++ src/app/admin/products/create/page.tsx
@@ -0,0 +1,38 @@
+import FormInput from "@/components/form/form-input";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { faker } from "@faker-js/faker";
+
+const createProductAction = async (formData: FormData) => {
+  "use server";
+
+  const name: string = formData.get("name") as string;
+  console.log(name);
+};
+
+export default function CreateProductPage() {
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 15 });
+
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">criar produto</h1>
+      <div className="border p-8 rounded-md">
+        <form action={createProductAction}>
+          <FormInput
+            type="text"
+            name="name"
+            label="nome do produto"
+            defaultValue={name}
+            placeholder="Nome do produto"
+          />
+          <Button type="submit" size={"lg"}>
+            Criar
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
+}
