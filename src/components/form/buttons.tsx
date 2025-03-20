00366b695a628aa58676358182b5ed2ba2423fbe
gumeeee <gaxco123@gmail.com>
2025-03-20 01:11:50 -0300

    feat: adding radix-ui/react-icons and implemeting SubmitButton component

---
+++ src/components/form/buttons.tsx
@@ -0,0 +1,38 @@
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ReloadIcon } from "@radix-ui/react-icons";
+
+type btnSize = "default" | "lg" | "sm";
+
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+export default function SubmitButton({
+  className = "",
+  text = "submit",
+  size = "lg",
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      disabled={pending}
+      className={cn("capitalize", className)}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Por favor, aguarde...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
