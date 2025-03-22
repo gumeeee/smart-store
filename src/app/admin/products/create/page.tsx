e686cc2999841c590e4d44d64c8d6c3075448f27
gumeeee <gaxco123@gmail.com>
2025-03-22 02:30:01 -0300

    feat: add formContainer and createProductionAction

--- src/app/admin/products/create/page.tsx
+++ src/app/admin/products/create/page.tsx
@@ -1,36 +1,54 @@
+import SubmitButton from "@/components/form/buttons";
+import CheckboxInput from "@/components/form/checkbox-input";
+import FormContainer from "@/components/form/form-container";
 import FormInput from "@/components/form/form-input";
+import ImageInput from "@/components/form/image-input";
+import PriceInput from "@/components/form/prince-input";
+import TextAreaInput from "@/components/form/text-area-input";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { createProductAction } from "@/utils/actions";
 import { faker } from "@faker-js/faker";
-
-const createProductAction = async (formData: FormData) => {
-  "use server";
-
-  const name: string = formData.get("name") as string;
-};
 
 export default function CreateProductPage() {
   const name = faker.commerce.productName();
   const company = faker.company.name();
   const description = faker.lorem.paragraph({ min: 10, max: 15 });
+  const price = faker.commerce.price({ dec: 0 });
 
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">criar produto</h1>
       <div className="border p-8 rounded-md">
-        <form action={createProductAction}>
-          <FormInput
-            type="text"
-            name="name"
-            label="nome do produto"
-            defaultValue={name}
-            placeholder="Nome do produto"
+        <FormContainer action={createProductAction}>
+          <div className="grid gap-4 md:grid-cols-2 my-4">
+            <FormInput
+              type="text"
+              name="name"
+              label="nome do produto"
+              defaultValue={name}
+              placeholder={"Nome do produto"}
+            />
+            <FormInput
+              type="text"
+              name="company"
+              label="empresa"
+              defaultValue={company}
+              placeholder={"Nome da empresa"}
+            />
+            <PriceInput defaultValue={Number(price)} />
+            <ImageInput />
+          </div>
+          <TextAreaInput
+            name="description"
+            labelText="descrição do produto"
+            defaultValue={description}
           />
-          <Button type="submit" size={"lg"}>
-            Criar
-          </Button>
-        </form>
+
+          <div className="mt-6">
+            <CheckboxInput name="featured" label="favorito" />
+          </div>
+          <SubmitButton text="criar produto" className="mt-8" />
+        </FormContainer>
       </div>
     </section>
   );
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import db from "@/utils/db";
 import { redirect } from "next/navigation";
 
@@ -39,4 +41,11 @@ export const fetchProductById = async (productId: string) => {
   if (!product) redirect("/products");
 
   return product;
+};
+
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "Produto Criado" };
 };
